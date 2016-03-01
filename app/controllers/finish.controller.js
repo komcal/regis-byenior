e9c5c3d6c7b8d6d8d2ca31390d641d98628fb91b
komcal <kom.sujautra@gmail.com>
2016-03-01 13:58:34 +0700

    confirm regis

--- app/controllers/finish.controller.js
+++ app/controllers/finish.controller.js
@@ -1,3 +1,9 @@
 exports.render = function(req, res){
-  res.render('finish');
+  var id = req.body.name;
+  var list = 'year'+(8-id[1]+1);
+  var redis = require('../../config/redis');
+  redis.setDataById(list,id,'come','1', function(){
+    res.render('finish');
+  });
+
 }
--- config/redis.js
+++ config/redis.js
@@ -4,15 +4,40 @@ exports.findById = function(list,id,callback){
   client.lrange(list, 0, -1, function(err, students){
     if(err) console.log("ERROR");
     else{
-      students = JSON.parse(students);
       var data = find(id,'id',students);
-      client.quit();
       callback(data);
+    }
+  });
+}
+exports.setDataById = function(list,id,field,newdata,callback){
+  client.lrange(list, 0, -1, function(err, students){
+    if(err) console.log("ERROR in confirm");
+    else{
+      var i=0;
+      for(student of students){
+        student = JSON.parse(student);
+        if(student['id'] == id){
+          student[field] = newdata;
+          console.log(student);
+          var data = JSON.stringify(student);
+          client.LSET(list,i,data, function(err,reply){
+            if(err)
+              console.log(err)
+            else {
+              console.log(reply)
+            }
+          });
+          break;
+        }
+        i++;
+      }
+      callback();
     }
   });
 }
 function find(data,field,items){
   for(var item of items){
+    item = JSON.parse(item);
     if(item[field] == data){
       return item;
     }
